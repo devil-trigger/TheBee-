0f51949bf0beb3a84e3b5687b4171e8ba58089a6
The bee <1242847392@qq.com>
2020-07-01 00:05:45 +0800

    增加了 页面进度条功能

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 $(function(){
-	let SurplusHeight=$(document).height() - $('body,html').scrollTop() - $(window).height();
-	let ScreenHeight=$(window).height();
-	let fenzi = SurplusHeight+ScreenHeight
-	// console.log(DocumentHeight)
-
+	// let SurplusHeight=$(document).height() - $(window).height();
+	// let ScreenHeight=$(window).height();
+	// let fenzi = SurplusHeight+ScreenHeight
+	// // console.log(DocumentHeight)
+	let femu = $(document).height() - $(window).height()
 	$(window).scroll(function () {
 	    //滚动事件代码
-			console.log(SurplusHeight)
+		let fenzi = $('body,html').scrollTop()
+		let data= (fenzi/femu).toFixed(2);
+		console.log(data*100)
+		$('.thebee-bar').css("width",data*100+"%")
 	})
 })
