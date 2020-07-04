a4a2655cad6c1f22e9e335f07b27581df35bf5d0
thebee <320975636@qq.com>
2020-07-04 17:24:43 +0800

    为p标签加上了时间，解决了多次点击按钮下滑的bug，初步完成后面时间的模模块

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@ $(function(){
 	$(window).scroll(function () {
 		let fenzi = $('body,html').scrollTop()
 		let data= (fenzi/femu).toFixed(2);
-		console.log(data*100)
+		// console.log(data*100)
 		$('.thebee-bar').css("width",data*100+"%")
 	})
 	let windowHeight=$(window).height()
-	console.log($('.position-sticky').height())
+	// console.log($('.position-sticky').height())
 	let positionStickHei=$('.position-sticky').height()
 	$('.swiper-container').css('height',windowHeight-positionStickHei+"px")
+	// --------获取时间 并丢进 id为 time 的标签内---------------------
 	$(function() {
 	    var myDate = new Date;
 	    var year = myDate.getFullYear(); //获取当前年
@@ -23,13 +24,18 @@ $(function(){
 	    // console.log(year, mon, date, weeks[week])
 	    $("#time").html(year + "年" + "&nbsp;" + mon + "月" + "&nbsp;" + date + "日" + "&nbsp;&nbsp;&nbsp;" +weeks[week]);
 	})
-	// console.log($('.bottom-down>span'))
+
+	// 点击按钮下滑顶部
+	// console.log($(window).scrollTop())
 	$('.bottom-down>span').click(function(){
-		$('html,body').animate({
-			"scrollTop":windowHeight,
-			
-		},700)
-		
+		// 作出判定  防止多次点击 无法上滑或者上滚动
+		if ($(window).scrollTop()==0) {
+			$('html,body').stop().animate({
+				"scrollTop":windowHeight,
+			},700)
+		} else{
+			return
+		}
 	})
 	
 })
---
+++ js/swiper-setting.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+			spaceBetween: 30,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		});
+// --------------------------------------------------------
+var swiper = new Swiper('.swiper-container-order', {
+	slidesPerView: 5,
+	spaceBetween: 10,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
